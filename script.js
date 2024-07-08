1528a73089257d7e3b96ea828a3cbbce31e23385
Donald Zarura <150556665+doniecode@users.noreply.github.com>
2024-07-08 06:38:27 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-function showSidebar(){
-  const sidebar = document.querySelector('.sidebar');
-  const menuBtn = document.querySelector('.menu-btn');
-  sidebar.style.display='flex';
-  menuBtn.style.display="none";
-  const expanded = menuBtn.firstElementChild;
-  if(expanded.getAttribute('aria-expanded')=='false'){
-    expanded.setAttribute('aria-expanded', 'true');
-  } else{
-    expanded.setAttribute('aria-expanded', 'false');
-  }
-  
-}
+//mobile sidebar
+const menubtns = document.querySelector(".menu-btns");
+const hamburger = document.querySelector(".hamburger");
+const closemenu = document.querySelector(".close-menu");
+const sidebar = document.querySelector(".sidebar");
 
-function hideSidebar(){
-  const sidebar = document.querySelector('.sidebar');
-  const menuBtn = document.querySelector('.menu-btn');
-  sidebar.style.display='none';
-  menuBtn.style.display="block";
-}
+hamburger.addEventListener("click", function(){
+  sidebar.style.display="flex";
+  hamburger.style.display="none";
+  closemenu.style.display="block";
+})
 
-function showDropdownhtml(){
-  const dropdown = document.querySelector('.html-content');
-  dropdown.style.display='flex';
-}
-
-let hidedropdownhtml;
-document.querySelector('.html-content svg').onclick = function(){
-  let hidedropdownhtml = document.querySelector('.html-content').style.display='none';
-}
-
-function showDropdowncss(){
-  const dropdown = document.querySelector('.css-content');
-  dropdown.style.display='flex';
-}
-
-let hidedropdowncss;
-document.querySelector('.css-content svg').onclick = function(){
-  let hidedropdowncss = document.querySelector('.css-content').style.display='none';
-}
-
-function showDropdownjs(){
-  const dropdown = document.querySelector('.js-content');
-  dropdown.style.display='flex';
-}
-
-let hidedropdownjs;
-document.querySelector('.js-content svg').onclick = function(){
-  let hidedropdownjs = document.querySelector('.js-content').style.display='none';
-} 
-
+closemenu.addEventListener("click", function(){
+  sidebar.style.display="none";
+  hamburger.style.display="block";
+  closemenu.style.display="none";
+})
